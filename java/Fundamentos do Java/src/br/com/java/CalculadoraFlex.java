a98290b6a1de52399e3e7fb2fe28b7741763be0e
Robson Carvalho <rbs.carva@gmail.com>
2018-12-28 19:23:17 -0200

    identação

--- java/Fundamentos do Java/src/br/com/java/CalculadoraFlex.java
+++ java/Fundamentos do Java/src/br/com/java/CalculadoraFlex.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 public class CalculadoraFlex {
 
 	public static void main(String[] args) {
-		//variáveis
+		
+		// variáveis
 		double alcool, gasolina;
+		
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Calculadora FLEX");
-		//entrada
+		
+		// entrada
 		System.out.print("Digite o valor do álcool: ");
 		alcool = teclado.nextDouble();
+		
 		System.out.print("Digite o valor da gasolina: ");
 		gasolina = teclado.nextDouble();
-		//processamento / saída
+		
+		// processamento / saída
 		if (alcool < 0.7 * gasolina) {
 			System.out.println("Abasteça com Álcool");
 		} else {
 			System.out.println("Abasteça com Gasolina");
 		}
-				
+
 	}
 }
